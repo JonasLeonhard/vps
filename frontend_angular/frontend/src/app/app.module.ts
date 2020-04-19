7f8318f065b13412545e17fc281af8084257779a
JonasLeonhard <jonasleonhardfl@gmail.com>
2020-04-19 17:55:50 +0200

    feat(cv): added skills.component
    Added skills component.
    Added tabs for skills
    Added fadein animation for skill bubbles
    Added styling
    Added some initial icons WIP
    
    References #skills

--- frontend_angular/frontend/src/app/app.module.ts
+++ frontend_angular/frontend/src/app/app.module.ts
@@ -1,29 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { Routes, RouterModule } from '@angular/router';
+import {AppComponent} from './app.component';
+import {RouterModule, Routes} from '@angular/router';
 
-import { NavigationComponent } from './navigation/navigation.component';
-import { FooterComponent } from './footer/footer.component';
-import { CvComponent } from './pages/cv/cv.component';
-import { Error404Component } from './error404/error404.component';
+import {NavigationComponent} from './navigation/navigation.component';
+import {FooterComponent} from './footer/footer.component';
+import {CvComponent} from './pages/cv/cv.component';
+import {Error404Component} from './error404/error404.component';
+import {SkillsComponent} from './pages/cv/skills/skills.component';
 
 const routes: Routes = [
-  { path:  '', redirectTo:  'cv', pathMatch:  'full' },
+  {path: '', redirectTo: 'cv', pathMatch: 'full'},
   {
     path: 'cv',
     component: CvComponent
   },
-  { path: '**', pathMatch: 'full', component: Error404Component },
+  {path: '**', pathMatch: 'full', component: Error404Component},
 ];
+
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
     FooterComponent,
     CvComponent,
-    Error404Component
+    Error404Component,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
