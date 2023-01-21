3c3b707f06db7f3a83f9d1b845cab76b96798d94
Jonas Leonhard <jonasleonhardfl@gmail.com>
2023-01-21 23:55:15 +0100

    feat(tailwind.config): added tailwindcss, added prettier plugins
    - added tailwindcss
    - added order-imports prettier plugin
    - added tailwind prettier plugin

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<slot />
+
+<style global lang="postcss">
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+</style>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,5 +2,7 @@
 	let name = 'test';
 </script>
 
-<h1>Welcome to SvelteKit {name}</h1>
+<h1 class="text-3xl font-bold underline">
+	Hello {name}!
+</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
