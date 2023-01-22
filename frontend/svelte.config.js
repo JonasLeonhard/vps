69a1a14dc0c1b6164fe02249514b0a2452897c4e
Jonas Leonhard <jonasleonhardfl@gmail.com>
2023-01-22 01:23:51 +0100

    fix(svelte.config.js): added resolve alias

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,6 +1,6 @@
-import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,7 +14,12 @@ const config = {
 	],
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		// aliases have to be added to tsconfig.json
+		alias: {
+			'@src': './src',
+			'@content': './content'
+		}
 	}
 };
 
