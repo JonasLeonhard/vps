acede18d2795b9e0931e56f3d3441a9a9b23fb27
Jonas Leonhard <jonasleonhardfl@gmail.com>
2023-01-23 14:48:01 +0100

    fix(default.php): added kirbytext

--- backend/site/templates/default.php
+++ backend/site/templates/default.php
@@ -1,1 +1,8 @@
-<h1><?= $page->title() ?></h1>
+<article>
+  <header>
+    <h1><?= $page->title() ?></h1>
+  </header>
+  <div class="text">
+    <?= $page->text()->kirbytext() ?>
+  </div>
+</article>
