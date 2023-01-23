d5d012fa3ea6c03ddcee2ddc3ff029c063fb8dc2
Jonas Leonhard <jonasleonhardfl@gmail.com>
2023-01-23 15:53:17 +0100

    fix(.gitignore, .env): added gitignore + .env

--- backend/index.php
+++ backend/index.php
@@ -2,4 +2,10 @@
 
 require 'kirby/bootstrap.php';
 
+use Dotenv\Dotenv;
+
+# Load .env file
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
 echo (new Kirby)->render();
--- backend/site/config/config.php
+++ backend/site/config/config.php
@@ -1,6 +1,11 @@
 <?php
 
 return [
+  'debug' => $_ENV['isLocal'] === 'true', # only for local-development
+  'api' => [
+    'basicAuth' => true,
+    'allowInsecure' => $_ENV['isLocal'] === 'true' # only for local-development
+  ],
   'languages' => [
     'detect' => true
   ]
