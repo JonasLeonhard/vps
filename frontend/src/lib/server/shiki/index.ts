2a6071c1aaa4e40ebe67c905ff8e2d8a86a3544c
unknown <jonasleonhardfl@gmail.com>
2023-06-12 00:06:49 +0200

    fix(package.json): removed unused deps

--- frontend/src/lib/server/shiki/index.ts
+++ frontend/src/lib/server/shiki/index.ts
@@ -5,6 +5,5 @@ import shikiConfig from './shiki.config';
 export default async function codeToHtml(lang: string, code: string) {
 	const highlighter = await getHighlighter(shikiConfig as any);
 
-	console.log(lang, code);
 	return highlighter.codeToHtml(code, { lang });
 }
