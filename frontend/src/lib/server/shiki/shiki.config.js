81e3c57eb2cd0495b13449910d699d6d099bfaca
unknown <jonasleonhardfl@gmail.com>
2023-06-12 21:51:02 +0200

    feat(core, theme): updated all packages to latest, fixed shiki not rendering when build, added catppuccin-mocha theme from compiled branch

--- frontend/src/lib/server/shiki/shiki.config.js
+++ frontend/src/lib/server/shiki/shiki.config.js
@@ -1,5 +1,8 @@
+// from catppuccin compiled branch: https://github.com/catppuccin/vscode/blob/compiled/mocha.json
+import theme from './catppuccin-mocha.json';
+
 const config = {
-	theme: 'material-theme-darker'
+	theme
 };
 
 export default config;
