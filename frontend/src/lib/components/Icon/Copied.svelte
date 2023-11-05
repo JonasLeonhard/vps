093eee4cb3588c1497de2c9db4a12ced3ab48770
jonas <jonasleonhardfl@gmail.com>
2023-11-05 14:54:58 +0100

    fix(icon): changed copy & copied icon

--- frontend/src/lib/components/Icon/Copied.svelte
+++ frontend/src/lib/components/Icon/Copied.svelte
@@ -1,9 +1,16 @@
-<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-	<path d="M15 6H3V8H15V6Z" fill="currentColor" />
-	<path d="M15 10H3V12H15V10Z" fill="currentColor" />
-	<path d="M3 14H11V16H3V14Z" fill="currentColor" />
-	<path
-		d="M11.9905 15.025L13.4049 13.6106L15.526 15.7321L19.7687 11.4895L21.1829 12.9037L15.526 18.5606L11.9905 15.025Z"
-		fill="currentColor"
-	/>
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width="24"
+	height="24"
+	viewBox="0 0 24 24"
+	stroke-width="1"
+	stroke="currentColor"
+	fill="none"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+>
+	<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+	<path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" />
+	<path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z" />
+	<path d="M9 14l2 2l4 -4" />
 </svg>
--- frontend/src/lib/components/Icon/Copy.svelte
+++ frontend/src/lib/components/Icon/Copy.svelte
@@ -1,11 +1,18 @@
-<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-	<path d="M13 7H7V5H13V7Z" fill="currentColor" />
-	<path d="M13 11H7V9H13V11Z" fill="currentColor" />
-	<path d="M7 15H13V13H7V15Z" fill="currentColor" />
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width="24"
+	height="24"
+	viewBox="0 0 24 24"
+	stroke-width="1"
+	stroke="currentColor"
+	fill="none"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+>
+	<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+	<path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h3m9 -9v-5a2 2 0 0 0 -2 -2h-2" />
 	<path
-		fill-rule="evenodd"
-		clip-rule="evenodd"
-		d="M3 19V1H17V5H21V23H7V19H3ZM15 17V3H5V17H15ZM17 7V19H9V21H19V7H17Z"
-		fill="currentColor"
+		d="M13 17v-1a1 1 0 0 1 1 -1h1m3 0h1a1 1 0 0 1 1 1v1m0 3v1a1 1 0 0 1 -1 1h-1m-3 0h-1a1 1 0 0 1 -1 -1v-1"
 	/>
+	<path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z" />
 </svg>
